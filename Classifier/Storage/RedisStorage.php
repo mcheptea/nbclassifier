419258d7fd6d9e98ea26055c58440aec72630ab8
Mark Cheptea <m.celmare@gmail.com>
2016-04-27 23:46:00 +0200

    Implemented method for storage and statistics.

--- Classifier/Storage/RedisStorage.php
+++ Classifier/Storage/RedisStorage.php
@@ -66,7 +66,7 @@ class RedisStorage {
     }
 
     /**
-     * The number of documents in a class
+     * Returns the number of documents in a class
      *
      * @param $class The class name
      * @return int The number of documents
@@ -99,5 +99,113 @@ class RedisStorage {
         }
 
         return $total;
+    }
+
+
+    /**
+     * Persists words and their corresponding clsasses.
+     *
+     * @param $word The word
+     * @param $class The class
+     */
+    public function addWord($word, $class)
+    {
+        //increment class:word
+        if ($this->redis->hexists("words", $class."#".$word)) {
+            $this->redis->hincrby("words", $class."#".$word, 1);
+        } else {
+            $this->redis->hset("words", $class."#".$word, 1);
+        }
+
+        //increment classWordCount
+        if ($this->redis->hexists("classWordCount", $class)) {
+            $this->redis->hincrby("classWordCount", $class, 1);
+        } else {
+            $this->redis->hset("classWordCount", $class, 1);
+        }
+
+        //increment vocabulary (number of unique words)
+        if (!$this->redis->hexists("vocabulary", $word)) {
+            $this->redis->hset("vocabulary", $word, 0);
+        } else {
+            $this->redis->hincrby("vocabulary", $word, 1);
+        }
+    }
+
+    /**
+     * Removes word from storage.
+     *
+     * @param $word
+     * @param $class
+     */
+    public function removeWord($word, $class)
+    {
+        //decrement class:word
+        if ($this->redis->hExists("words", $class."#".$word)) {
+            $this->redis->hIncrBy("words", $class."#".$word, -1);
+
+            if ($this->redis->hGet("words", $class."#".$word) <= 0) {
+                $this->redis->hDel("words", $class."#".$word);
+            }
+        }
+
+        //decrement classWordCount
+        if ($this->redis->hexists("classWordCount", $class)) {
+            $this->redis->hincrby("classWordCount", $class, -1);
+
+            if ($this->redis->hGet("classWordCount", $class) <= 0) {
+                $this->redis->hDel("classWordCount", $class);
+            }
+        }
+
+        //decrement vocabulary
+        if ($this->redis->hexists("vocabulary", $word)) {
+            $this->redis->hincrby("vocabulary", $word, -1);
+        }
+    }
+
+    /**
+     * Returns the number of word occurrences in class.
+     *
+     * @param $word
+     * @param $class
+     * @return int The number of occurrences of a word in class.
+     */
+    public function countWordInClass($word, $class)
+    {
+        if ($this->redis->hexists("words", $class."#".$word)) {
+            return $this->redis->hget("words", $class."#".$word);
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Returns the total number of words in a given class.
+     *
+     * @param $class
+     * @return int the number of words in the class
+     */
+    public function countWordsInClass($class)
+    {
+        if ($this->redis->hexists("classWordCount", $class)) {
+            return $this->redis->hget("classWordCount", $class);
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Returns the number of words in the vocabulary.
+     *
+     * @return int The vocabulary size
+     */
+    public function countVocabulary()
+    {
+        if ($this->redis->exists("vocabulary")) {
+            return $this->redis->hlen("vocabulary");
+        } else {
+            return 0;
+        }
     }
 }
