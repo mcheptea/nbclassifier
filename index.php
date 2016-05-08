2b08fb5da4f389bd4d19f1f4f02da9b227bc8ab4
Mark Cheptea <m.celmare@gmail.com>
2016-05-08 23:15:30 +0300

    #2 Refactored the package to make it more composer friendly.

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-ini_set('memory_limit','512M');
-
-//autoload
-require_once("vendor/autoload.php");
-
--- src/Classifier/Config/Config.php
+++ src/Classifier/Config/Config.php
@@ -14,7 +14,7 @@ use Exception;
  */
 class Config
 {
-    public static $configFile = "../../config.yml";
+    public static $configFile = "../../../nbclassifier.yml";
 
     /**
      * Returns the value of a key. The path is separated by dots (.)
