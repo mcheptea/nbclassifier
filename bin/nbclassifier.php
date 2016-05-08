abebf94c2d12cbfc623f7c6f43250709e2b974f5
Mark Cheptea <m.celmare@gmail.com>
2016-05-08 23:16:38 +0300

    #2 Created executable stubs for the trainer and the classifier.

---
+++ bin/nbclassifier.php
@@ -0,0 +1,11 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Naive Bayes Classifier (CLI utility)
+ *
+ * User: Mark Cheptea
+ * Date: 08-May-16
+ * Time: 21:53
+ */
+
+die("todo!");
---
+++ bin/nbtrainer.php
@@ -0,0 +1,11 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Naive Bayes Classifier Trainer (CLI utility)
+ *
+ * User: Mark Cheptea
+ * Date: 08-May-16
+ * Time: 21:56
+ */
+
+die("todo!");
